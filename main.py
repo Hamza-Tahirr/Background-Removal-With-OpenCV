ac9fb5ee874f24f42673b10b4fe4370d4275980a
Hamza Tahir <90556877+Hamza-Tahirr@users.noreply.github.com>
2022-09-10 16:30:54 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sre_constants import SUCCESS
 import cv2
 import cvzone
 from cv.zone.SelfiSegmentationModule import SelfiSegmentation
@@ -5,3 +6,13 @@ import os
 
 cap =cv2.VideoCapture(0)
 cap.set(3,648)
+cap.set(4, 488)
+segmentor= SelfiSegmentation() #for landscape 1
+
+while True:
+    success, img = cap.read()
+    imgOut = segmentor.removeBG(img,(255,0,255))
+
+    cv2.imshow("image",img)
+    cv2.imshow('Image Out',imgOut)
+    cv2.waitKey(1)
